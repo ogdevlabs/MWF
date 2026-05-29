f38e722788c16ecf7d276ca10c442b0f440ad3e5
Oscar Paul Garcia <oscargarcia@ogdevlabs.onmicrosoft.com>
2026-05-29 10:14:48 -0600

    feat(07-03): wire Firebase.initializeApp into main.dart
    - Add firebase_core and firebase_options.dart imports
    - Replace TODO Phase 7 comment with actual Firebase.initializeApp() call
    - FCM background handler and FcmService.initialize() will be called post-auth

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'firebase_options.dart';
 import 'shared/theme/app_theme.dart';
 import 'shared/router/app_router.dart';
 
@@ -26,8 +28,8 @@ void main() async {
     await Purchases.configure(PurchasesConfiguration(rcApiKey));
   }
 
-  // TODO Phase 7: Initialize Firebase
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Firebase initialization for push notifications (FCM)
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: MwfApp()));
 }
